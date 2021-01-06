19623b08ecd46c3657798260c9f23a28a1fbb13d
danialpower01 <danialhaikal980115@gmail.com>
2021-01-06 17:12:19 +0800

    Card

--- lib/pages/BlogPage.dart
+++ lib/pages/BlogPage.dart
@@ -19,10 +19,11 @@ class _BlogPageState extends State<BlogPage> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          _topPageTitle(),
+          _topPageTitle(), _cardTest()
         ],
       )
     );
+    
   }
 
   Widget _topPageTitle() {
@@ -53,6 +54,36 @@ class _BlogPageState extends State<BlogPage> {
       ),
     );
   }
+  
+  Widget _cardTest() {
+    return Center(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const ListTile(
+              leading: Icon(Icons.album),
+              title: Text('The Enchanted Nightingale'),
+              subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Text('BUY TICKETS'),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+                TextButton(
+                  child: const Text('LISTEN'),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
-
-
