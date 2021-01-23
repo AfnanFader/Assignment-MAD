7d792cb8a9026db56f22978acbe0e17621c6a67c
Zaidan <zaidanzeta98@gmail.com>
2021-01-23 23:25:27 +0800

    finalize my part xD

---
+++ lib/model/pet.dart
@@ -0,0 +1,86 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Pet {
+
+  String uidCreator;
+  String docID;
+
+  List<dynamic> images = [];
+  String petName;
+  String type;
+  double age;
+  String location;
+  String breed;
+  bool vaccine;
+  String cacat;
+  bool gender;
+  String info;
+
+  List likedUsers;
+  String phoneNumber;
+  Timestamp dateCreated;
+
+
+  Pet({
+    this.uidCreator, 
+    this.docID, 
+    this.petName, 
+    this.type, 
+    this.age, 
+    this.location, 
+    this.breed,
+    this.vaccine,
+    this.cacat,
+    this.gender,
+    this.info,
+    this.likedUsers,
+    this.phoneNumber,
+    this.dateCreated,
+  });
+
+
+  set setPetImages(List data) {
+    images = data;
+  }
+
+
+  Map<String, dynamic> toMap() {
+    return {
+      'uidCreator' : uidCreator,
+
+      'PetImagesLink' : images,
+      'PetName' : petName,
+      'TypeOfPet' : type,
+      'Age' : age,
+      'Location' : location,
+      'Disabilities' : cacat,
+      'Gender' : gender,
+      'AdditionalInfo' : info,
+
+      'LikedUsers' : likedUsers,
+      'PhoneNumber' : phoneNumber,
+      "DateCreated" : dateCreated,
+    };
+  }
+
+
+  Pet.fromMap(Map<String, dynamic> data, String documentID) {
+    uidCreator = data['uidCreator'];
+
+    images = data['PetImagesLink'];
+    petName = data['PetName'];
+    type = data['TypeOfPet'];
+    age = data['Age'];
+    location = data['Location'];
+    cacat = data['Disabilities'];
+    gender = data['Gender'];
+    info = data['AdditionalInfo'];
+
+    likedUsers = data['LikedUsers'];
+    phoneNumber = data['PhoneNumber'];
+    dateCreated = data["DateCreated"];
+    docID = documentID;
+  }
+
+
+}
--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,19 +1,25 @@
 
+import 'dart:typed_data';
+
 import 'package:assignment_project/model/blog.dart';
+import 'package:assignment_project/model/pet.dart';
 import 'package:assignment_project/model/user.dart';
+import 'package:assignment_project/notifier/UserNotifier.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:multi_image_picker/multi_image_picker.dart';
+import 'package:random_string/random_string.dart';
 
 class DatabaseService {
 
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
+  CollectionReference petPostsCollectionRef = FirebaseFirestore.instance.collection('PetPosts');
 
   //INITIALIZATION & ONUPDATE PROFILE
   Future<UserDetail> getUserData(String uid) {
     return _firestore.collection('User').doc(uid).get()
     .then((DocumentSnapshot snapshot) => UserDetail.fromMap(snapshot.data()));
   }
-
 
   //blog trending
   Stream<List<BlogTrending>> getBlogTrending() {
@@ -35,5 +41,63 @@ class DatabaseService {
     .snapshots()
     .map((event) => event.docs.map((e) => BlogDogs.fromMap(e.data())).toList());
   }
+
+  //submit Post of Pets
+
+
+
+
+  //Firebase Storage for Posts
+  Future<List<String>> uploadPetImage(List<Asset> asset, String creatorUID, String petname) async {
+
+    List<String> uploadUrl = [];
+
+    await Future.wait(
+        asset.map((Asset image) async {
+
+          String fileName = petname + randomAlphaNumeric(6); //name of file
+
+          ByteData byteData = await image.getByteData(quality: 100);
+          List<int> imageData = byteData.buffer.asUint8List();
+
+          StorageTaskSnapshot snapshot = await FirebaseStorage.instance.ref().child('/petPostImages/$creatorUID/$fileName')
+          .putData(imageData).onComplete;
+          
+          if (snapshot.error == null) {
+            String downloadUrl = await snapshot.ref.getDownloadURL();
+            uploadUrl.add(downloadUrl);
+            print('[Pet Post] Storage Success upload');
+          } else {
+            print('[Pet Post] Storage Error during upload : ${snapshot.error.toString()}');
+            throw ('something wrong here boi');
+          }
+        }),
+        eagerError: true,
+        cleanUp: (_) {
+          print('[Pet Post] Storage CleanUp Error');
+        });
+    return uploadUrl;
+  }
+
+
+  //Submit Post Pets
+  Future<bool> submitNewPetPost(Pet petData, UserNotifier userNotifier, List<Asset> asset) async {
+
+    try {
+      return Future.wait([uploadPetImage(asset, userNotifier.getUserUID, petData.petName)]).then((value) async {
+
+        petData.setPetImages = value[0];
+        return petPostsCollectionRef.add(petData.toMap()).then((doc) {        
+          print("[Pet Post] Succesfully upload Pet documents");
+          return true;
+        });
+      });
+    } catch (e) {
+      print("[Pet Post] Error in create Pet Post function : ${e.toString()}");
+      return false;
+    }
+  }
+
+
 
 }
